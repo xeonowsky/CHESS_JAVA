f9b9d0cccd9b843ef07aadc7a28b22f922404836
xeonowsky <klaudiusz599@gmail.com>
2024-04-05 00:35:48 +0200

    First commit

---
+++ Chess/src/Main.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("Chess Game");
+        frame.setLayout(new GridBagLayout());
+        frame.setMinimumSize(new Dimension(1000,1000));
+        frame.setLocationRelativeTo(null);
+
+        Board board=new Board();
+        frame.add(board);
+
+
+        frame.setVisible(true);
+
+
+
+    }
+}
