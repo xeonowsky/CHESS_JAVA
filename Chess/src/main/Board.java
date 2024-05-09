49bfcbea71b2ef1c18a010a6fddd3c2acae2041d
xeonowsky <klaudiusz599@gmail.com>
2024-05-10 00:44:05 +0200

    22 commit // Checkmate

--- Chess/src/main/Board.java
+++ Chess/src/main/Board.java
@@ -338,37 +338,37 @@ Piece findKing(boolean isWhite){
 
 
 public void addPiece(){
-//    pieceList.add(new Knight(this,1,0,false));
-//    pieceList.add(new Knight(this,6,0,false));
-//    pieceList.add(new Pawn(this,0,1,false));
-//    pieceList.add(new Pawn(this,1,1,false));
-//    pieceList.add(new Pawn(this,2,1,false));
-//    pieceList.add(new Pawn(this,3,1,false));
-//    pieceList.add(new Pawn(this,4,1,false));
-//    pieceList.add(new Pawn(this,5,1,false));
-//    pieceList.add(new Pawn(this,6,1,false));
-//    pieceList.add(new Pawn(this,7,1,false));
-//    pieceList.add(new Rook(this,0,0,false));
-//    pieceList.add(new Rook(this,7,0,false));
-//    pieceList.add(new Bishop(this,2,0,false));
-//    pieceList.add(new Bishop(this,5,0,false));
-//    pieceList.add(new Queen(this,3,0,false));
+    pieceList.add(new Knight(this,1,0,false));
+    pieceList.add(new Knight(this,6,0,false));
+    pieceList.add(new Pawn(this,0,1,false));
+    pieceList.add(new Pawn(this,1,1,false));
+    pieceList.add(new Pawn(this,2,1,false));
+    pieceList.add(new Pawn(this,3,1,false));
+    pieceList.add(new Pawn(this,4,1,false));
+    pieceList.add(new Pawn(this,5,1,false));
+    pieceList.add(new Pawn(this,6,1,false));
+    pieceList.add(new Pawn(this,7,1,false));
+    pieceList.add(new Rook(this,0,0,false));
+    pieceList.add(new Rook(this,7,0,false));
+    pieceList.add(new Bishop(this,2,0,false));
+    pieceList.add(new Bishop(this,5,0,false));
+    pieceList.add(new Queen(this,3,0,false));
     pieceList.add(new King(this,4,0,false));
 
-//    pieceList.add(new Knight(this,1,7,true));
-//    pieceList.add(new Knight(this,6,7,true));
-//    pieceList.add(new Pawn(this,0,6,true));
-//    pieceList.add(new Pawn(this,1,6,true));
-//    pieceList.add(new Pawn(this,2,6,true));
-//    pieceList.add(new Pawn(this,3,6,true));
-//    pieceList.add(new Pawn(this,4,6,true));
-//    pieceList.add(new Pawn(this,5,6,true));
-//    pieceList.add(new Pawn(this,6,6,true));
-//    pieceList.add(new Pawn(this,7,6,true));
-//    pieceList.add(new Rook(this,0,7,true));
-//    pieceList.add(new Rook(this,7,7,true));
-//    pieceList.add(new Bishop(this,2,7,true));
-//    pieceList.add(new Bishop(this,5,7,true));
+    pieceList.add(new Knight(this,1,7,true));
+    pieceList.add(new Knight(this,6,7,true));
+    pieceList.add(new Pawn(this,0,6,true));
+    pieceList.add(new Pawn(this,1,6,true));
+    pieceList.add(new Pawn(this,2,6,true));
+    pieceList.add(new Pawn(this,3,6,true));
+    pieceList.add(new Pawn(this,4,6,true));
+    pieceList.add(new Pawn(this,5,6,true));
+    pieceList.add(new Pawn(this,6,6,true));
+    pieceList.add(new Pawn(this,7,6,true));
+    pieceList.add(new Rook(this,0,7,true));
+    pieceList.add(new Rook(this,7,7,true));
+    pieceList.add(new Bishop(this,2,7,true));
+    pieceList.add(new Bishop(this,5,7,true));
     pieceList.add(new Queen(this,3,7,true));
     pieceList.add(new King(this,4,7,true));
 
